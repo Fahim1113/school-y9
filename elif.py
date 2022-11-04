cd639bfa0306e1fb669dc1e2c63f61b4f8b80d6e
Fahim1113 <fahimshahriyarc@gmail.com>
2022-11-04 19:57:37 +0000

    a

---
+++ elif.py
@@ -0,0 +1,12 @@
+#elif
+
+subject = str(input("what is your favourite subject? "))
+
+if subject == "computing":
+  print("correct")
+elif subject == "Computing":
+  print("correct")
+elif subject == "COMPUTING":
+  print("correct")
+else:
+  print("wrong")
---
+++ if.py
@@ -0,0 +1,14 @@
+#if
+name = str(input("what is your name? "))
+
+if name == "fahim":
+  print("Hello")
+else:
+  print("Go away")
+
+school = str(input("What school do you go to? "))
+
+if school == "lister":
+  print("Hello")
+else:
+  print("RUn away")
