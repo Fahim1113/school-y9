227c73bd11ab560cab4a3f332faa78aaae3e4142
FahimCHOWDHURY6 <>
2022-11-07 11:05:45 +0000

    A

---
+++ forloop.py
@@ -0,0 +1,7 @@
+# basic forloop
+for x in range(100, 0, -5):
+  print(x)
+
+#nine times table with forloop
+for x in range(1, 21):
+  print(str(x)+"*9="+str(x*9))
---
+++ main.py
@@ -0,0 +1,2 @@
+for x in range(1, 21):
+  print(str(x)+"*9="+str(x*9))
